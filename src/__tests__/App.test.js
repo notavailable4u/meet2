7dfe60f61addfdf3afb98591f84f612b609371fe
notavailable4u <139932183+notavailable4u@users.noreply.github.com>
2024-05-24 10:35:50 -0500

    Exercise-4-4

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,55 @@
+// src/__tests__/App.test.js
+
+import { render, within } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
+
+describe('<App /> component', () => {
+    let appDom;
+    beforeEach(() => {
+        appDom = render(<App/>).container.firstChild;
+    });
+   test('renders list of events', () => {
+    expect(appDom.querySelector('#event-list')).toBeInTheDocument();
+   });
+
+   test('render CitySearch', () => {
+    expect(appDom.querySelector('#city-search')).toBeInTheDocument();
+    });
+
+   test('render NumberOfEvents', () => {
+    expect(appDom.querySelector('#numberOfEvents')).toBeInTheDocument();
+   });
+});
+
+
+
+describe('<App /> integration', () => {
+    test('renders a list of events matching the city selected by the user', async () => {
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+    
+        const CitySearchDOM = AppDOM.querySelector('#city-search');
+        const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+    
+        await user.type(CitySearchInput, "Berlin");
+        const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+        await user.click(berlinSuggestionItem);
+    
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');   
+    
+        const allEvents = await getEvents();
+        const berlinEvents = allEvents.filter(
+          event => event.location === 'Berlin, Germany'
+        );
+    
+        expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+        allRenderedEventItems.forEach(event => {
+            expect(event.textContent).toContain("Berlin, Germany");
+          });
+      });
+});
+
